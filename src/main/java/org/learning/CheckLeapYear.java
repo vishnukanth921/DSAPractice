de42db31ffe7765cc30b452eb60f923fb3c2ba3c
vishnukanth921 <vishnukanthreddy921@gmail.com>
2025-10-30 16:48:15 +0530

    Initial commit

---
+++ src/main/java/org/learning/CheckLeapYear.java
@@ -0,0 +1,18 @@
+package org.learning;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year to check leap year or not: ");
+        int year = sc.nextInt();
+        if(year % 4 == 0 && year % 400 ==0){
+            System.out.println("leap year");
+        }else if (year % 100 !=0) {
+            System.out.println("leap year");
+        }else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
